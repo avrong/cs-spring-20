dc2e89206f10a6d8607863929d9acf48f74c4c0c
Aleksei Trifonov <avrong@pm.me>
2020-03-05 01:09:58 +0300

    Added some stuff

---
+++ Web/Day in History/data.js
@@ -0,0 +1,67 @@
+var events = [
+    { date: "0000-00-00", name: "Страны", link: "http://fivt.chuvsu.ru/kurs2/outCountries/index.html" },
+    { date: "0000-00-00", name: "Imagine Dragons", link: "http://fivt.chuvsu.ru/kurs2/IDr2008/index.html" },
+
+    { date: "0000-01-15", name: "День рождения Wikipedia", link: "http://fivt.chuvsu.ru/kurs2/011501/index.html" },
+
+    { date: "0000-02-02", name: "Победа в Сталинградской битве", link: "http://fivt.chuvsu.ru/kurs2/020201/index.html" },
+
+    { date: "0000-03-03", name: "День глинтвейна", link: "http://fivt.chuvsu.ru/kurs2/030301/index.html" },
+    { date: "0000-03-04", name: "Масленица", link: "http://fivt.chuvsu.ru/kurs2/030401/index.html" },
+    { date: "0000-03-08", name: "Международный женский день", link: "http://fivt.chuvsu.ru/kurs2/030801/index.html" },
+    { date: "0000-03-08", name: "Февральская революция", link: "http://fivt.chuvsu.ru/kurs2/030802/index.html" },
+    { date: "0000-03-08", name: "Международный женский день (Юля)", link: "http://fivt.chuvsu.ru/kurs2/030803/index.html" },
+    { date: "0000-03-15", name: "Международный день сна", link: "https://tanyaanya.github.io/weblab2/" },
+
+    { date: "0000-04-01", name: "День смеха", link: "http://fivt.chuvsu.ru/kurs2/040101/index.html" },
+    { date: "0000-04-12", name: "День космонавтики (Михаил Васильевич)", link: "http://fivt.chuvsu.ru/kurs2/041203/index1.html" },
+    { date: "0000-04-12", name: "День космонавтики", link: "http://fivt.chuvsu.ru/kurs2/041201/index.html" },
+    { date: "0000-04-12", name: "День космонавтики", link: "http://fivt.chuvsu.ru/kurs2/041202/index.html" },
+    { date: "0000-04-12", name: "День космонавтики", link: "http://fivt.chuvsu.ru/kurs2/041204/1204_CosmosDay/index.html" },
+    { date: "0000-04-19", name: "Первая пасхалка в игре", link: "http://fivt.chuvsu.ru/kurs2/041901/index.html" },
+    { date: "0000-04-26", name: "Авария на Чернобыльской АЭС", link: "http://fivt.chuvsu.ru/kurs2/042601/index.html" },
+    { date: "0000-04-29", name: "Международный день танца", link: "http://fivt.chuvsu.ru/kurs2/042901/3index.html" },
+
+    { date: "0000-05-01", name: "День труда", link: "http://fivt.chuvsu.ru/kurs2/050101/index.html" },
+    { date: "0000-05-01", name: "Праздник Весны и Труда", link: "http://fivt.chuvsu.ru/kurs2/050102/index.html" },
+    { date: "0000-05-01", name: "День труда", link: "http://fivt.chuvsu.ru/kurs2/050103/index.html" },
+    { date: "0000-05-01", name: "День труда (Эльдар)", link: "http://fivt.chuvsu.ru/kurs2/050104/index.html" },
+    { date: "0000-05-09", name: "День Победы", link: "http://fivt.chuvsu.ru/kurs2/050901/index.html" },
+    { date: "0000-05-15", name: "Международный день семьи", link: "http://fivt.chuvsu.ru/kurs2/051501/index.html" },
+
+    { date: "0000-06-10", name: "День мороженого ", link: "http://fivt.chuvsu.ru/kurs2/061001/index.html" },
+    { date: "0000-06-12", name: "День России ", link: "http://fivt.chuvsu.ru/kurs2/061201/index.html" },
+    { date: "0000-06-22", name: "Оборона Брестской крепости", link: "http://fivt.chuvsu.ru/kurs2/220641/index.html" },
+    { date: "0000-06-24", name: "Ивана Купала ", link: "http://fivt.chuvsu.ru/kurs2/062401/index.html" },
+
+    { date: "0000-07-02", name: "День уфолога", link: "http://fivt.chuvsu.ru/kurs2/070201/index.html" },
+    { date: "0000-07-12", name: "День Фотографа", link: "http://fivt.chuvsu.ru/kurs2/071201/index.html" },
+
+    { date: "0000-08-03", name: "День арбуза", link: "http://fivt.chuvsu.ru/kurs2/080301/index.html" },
+
+    { date: "0000-09-01", name: "День знаний", link: "http://fivt.chuvsu.ru/kurs2/090101/index.html" },
+    { date: "0000-09-13", name: "День программиста ", link: "http://fivt.chuvsu.ru/kurs2/091301/index.html" },
+    { date: "0000-09-13", name: "День программиста ", link: "http://fivt.chuvsu.ru/kurs2/091302/index.html" },
+    { date: "0000-09-13", name: "День Бэтмена ", link: "http://fivt.chuvsu.ru/kurs2/091301/index.html" },
+
+    { date: "0000-10-01", name: "Международный день кофе", link: "http://fivt.chuvsu.ru/kurs2/100101/index.html" },
+    { date: "0000-10-01", name: "Международный день музыки", link: "http://fivt.chuvsu.ru/kurs2/100102/index.html" },
+    { date: "0000-10-04", name: "Всемирный день защиты животных", link: "http://fivt.chuvsu.ru/kurs2/100401/index.html" },
+    { date: "0000-10-20", name: "День Повара", link: "http://fivt.chuvsu.ru/kurs2/201001/index.html" },
+    { date: "0000-10-25", name: "Всемирный день пасты", link: "http://fivt.chuvsu.ru/kurs2/102501/index.html" },
+    { date: "0000-10-28", name: "День автомобилиста", link: "http://fivt.chuvsu.ru/kurs2/102801/index.html" },
+    { date: "0000-10-31", name: "Хэллоуин (Можайкин С.П.)", link: "http://fivt.chuvsu.ru/kurs2/103101/My%20Web.html" },
+    { date: "0000-10-31", name: "Хэллоуин (Петров А.В.)", link: "http://fivt.chuvsu.ru/kurs2/103102/index.html" },
+
+    { date: "0000-11-01", name: "День Мертвых ", link: "http://fivt.chuvsu.ru/kurs2/110101/obshai.html" },
+    { date: "0000-11-02", name: "Всемирный день мужчин ", link: "http://fivt.chuvsu.ru/kurs2/110201/index.html" },
+    { date: "0000-11-04", name: "День народного единства", link: "http://fivt.chuvsu.ru/kurs2/110402/index.html" },
+    { date: "0000-11-04", name: "День народного единства", link: "http://fivt.chuvsu.ru/kurs2/110403/index.html" },
+    { date: "0000-11-17", name: "Международный день студента", link: "http://fivt.chuvsu.ru/kurs2/111701/index.html" },
+    { date: "0000-11-25", name: "Представление Юкио Мисимы", link: "http://fivt.chuvsu.ru/kurs2/112501/index.html" },
+
+    { date: "0000-12-07", name: "Международный день Гражданской авиации", link: "http://fivt.chuvsu.ru/kurs2/sashd.github.io" },
+    { date: "0000-12-10", name: "Международный день футбола", link: "http://fivt.chuvsu.ru/kurs2/091001/index.html" },
+    { date: "0000-12-15", name: "Международный день чая", link: "http://fivt.chuvsu.ru/kurs2/121501/index.html" },
+    { date: "0000-12-16", name: "Бостонское чаепитие", link: "http://fivt.chuvsu.ru/kurs2/121601/index.html" },
+]
