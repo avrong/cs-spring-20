a83a038b052e9bfc7e6b45399b6794c8e30092ce
Aleksei Trifonov <avrong@avrong.me>
2020-05-17 11:22:26 +0300

    Add object

--- arrays/index.js
+++ arrays/index.js
@@ -1,64 +1,4 @@
-const makeTableFromCountry = (c) => {
-    const baseParams = ['Континент', 'Часть света', 'Разница во времени', 'Денежная единица'];
-    return `<table width="100%" style="margin-bottom: 2em;">
-                <thead>
-                    <tr>
-                        <th colspan="2">
-                            <h1>${c[0]} — ${c[1]}</h1>
-                        </th>
-                    </tr>
-                </thead>
-
-                <tr>
-                    <td colspan="2">
-                        <table width="75%" style="margin: 0 auto;">
-                            ${
-                                baseParams.map((v, i) => {
-                                    return `<tr>
-                                                <td><b>${v}</b></td>
-                                                <td>${c[i+2]}</td>
-                                            </tr>`;
-                                }).join('\n')
-                            }
-                        </table>
-                    </td>
-                </tr>
-
-                ${
-                    c[6].map((v, i) => {
-                        return `<tr>
-                                    <td><b>${i == 0 ? "Географические объекты" : ""}</b></td>
-                                    <td>${i+1}. ${v}</td>
-                                </tr>`;
-                    }).join('\n')
-                }
-
-                ${
-                    c[7].sort((a, b) => a[2] < b[2] ? -1 : a[2] > b[2] ? 1 : 0).map((v, i) => {
-                        return `<tr>
-                                    <td><b>${i == 0 ? "Литературные произведения" : ""}</b></td>
-                                    <td>${i+1}. ${v[0]} «${v[1]}» (${v[2]})</td>
-                                </tr>`;
-                    }).join('\n')
-                }
-
-                <tr>
-                    <td colspan="2" style="text-align: center;"><b>Исторические события:</b></td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <table width="100%" border="1"><tr>
-                            ${
-                                Object.keys(c[8]).map((k, i) => {
-                                    return `<td colspan="2"><small><b>${k}</b> — ${c[8][k]}</small></td>`;
-                                }).join('\n')
-                            }
-                        </tr></table>
-                    </td>
-                </tr>
-            </table>`;
-};
-
+// TIMER //
 const makeTimer = () => {
     const date = new Date();
     const studyStartDate = new Date('2020-09-01');
@@ -95,6 +35,92 @@ const showTimer = () => {
 showTimer();
 setInterval(showTimer, 1000);
 
-countries.forEach((country) => {
-    return document.getElementById('content').innerHTML += makeTableFromCountry(country);
-});
+// COUNTRIES //
+const makeTableFromCountry = (c) => {
+    const baseParams = ['Континент', 'Часть света', 'Разница во времени', 'Денежная единица'];
+    return `<table width="100%" style="margin-bottom: 2em;">
+                <thead>
+                    <tr>
+                        <th colspan="2">
+                            <h1>${c[0]} — ${c[1]}</h1>
+                        </th>
+                    </tr>
+                </thead>
+
+                <tr>
+                    <td colspan="2">
+                        <table width="75%" style="margin: 0 auto;">
+                            ${
+                                baseParams.map((v, i) => {
+                                    return `<tr>
+                                                <td><b>${v}</b></td>
+                                                <td>${c[i+2]}</td>
+                                            </tr>`;
+                                }).join('\n')
+                            }
+                        </table>
+                    </td>
+                </tr>
+
+                ${
+                    c[6].map((v, i) => {
+                        return `<tr>
+                                    <td><b>${i == 0 ? "Географические объекты" : ""}</b></td>
+                                    <td>${i+1}. ${v}</td>
+                                </tr>`;
+                    }).join('\n')
+                }
+
+                ${
+                    c[7].map((v, i) => {
+                        return `<tr>
+                                    <td><b>${i == 0 ? "Литературные произведения" : ""}</b></td>
+                                    <td>${i+1}. ${v[0]} «${v[1]}» (${v[2]})</td>
+                                </tr>`;
+                    }).join('\n')
+                }
+
+                <tr>
+                    <td colspan="2" style="text-align: center;"><b>Исторические события:</b></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <table width="100%" border="1"><tr>
+                            ${
+                                Object.keys(c[8]).map((k, i) => {
+                                    return `<td colspan="2"><small><b>${k}</b> — ${c[8][k]}</small></td>`;
+                                }).join('\n')
+                            }
+                        </tr></table>
+                    </td>
+                </tr>
+            </table>`;
+};
+
+class Countries {
+    countries = [];
+
+    constructor(countries) {
+        this.countries = countries;
+    }
+
+    sorted() {
+        let _countries = JSON.parse(JSON.stringify(this.countries));
+        _countries = _countries.forEach((v, i, a) => a[i][7] = v[7].sort((a, b) => a[2] < b[2] ? -1 : a[2] > b[2] ? 1 : 0));
+        return new Countries(_countries);
+    }
+
+    generate() {
+        let html = '';
+
+        countries.forEach((country) => {
+            html += makeTableFromCountry(country);
+        });
+
+        return html;
+    }
+};
+
+const cns = new Countries(countries);
+const cnsSorted = cns.sorted();
+document.getElementById('content').innerHTML = cnsSorted.generate();
